ee48bfe03101cad22cb61d12ecd794c2faadcde5
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-03-10 21:11:30 +0530

    Type Aliases

--- notes.ts
+++ notes.ts
@@ -630,3 +630,26 @@ console.log(value.toString()); // myNumber: 10
 type Wrapped<T = string> = { value: T }; // T has an optional default value of string
 
 const wrappedValue: Wrapped<number> = { value: 10 };
+
+// -------------------Type Aliases--------------------
+
+// Type Aliases allow defining types with a custom name (an Alias).
+
+// Type Aliases can be used for primitives like string or more complex types such as objects and arrays:
+// type CarYear = number
+// type CarType = string
+// type CarModel = string
+// type Car = {
+//   year: CarYear,
+//   type: CarType,
+//   model: CarModel
+// }
+
+// const carYear: CarYear = 2001
+// const carType: CarType = "Toyota"
+// const carModel: CarModel = "Corolla"
+// const car: Car = {
+//   year: carYear,
+//   type: carType,
+//   model: carModel
+};
